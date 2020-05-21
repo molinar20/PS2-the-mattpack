1ca533364c08fb93319f4082145fd9bdd6cc00c2
Danielle Bovie <dbovie@login1.ufhpc>
2020-05-21 15:31:21 -0400

    updated Q3

--- problem_3.sh
+++ problem_3.sh
@@ -1,5 +1,8 @@
 #!/usr/bin
 
-awk '{print $1}' /ufrc/bsc4452/share/Class_Files/data/flights.May2017-Apr2018.csv | cut -f 3 -d "," | sort -u | sed "244d"
+airport_code () {
+	awk '{print $1}' /ufrc/bsc4452/share/Class_Files/data/flights.May2017-Apr2018.csv | cut -f 3 -d "," | sort -u | sed "244d"
 
+}
 
+airport_code
